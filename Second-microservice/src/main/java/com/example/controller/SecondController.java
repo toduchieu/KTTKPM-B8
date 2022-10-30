cae457d1c21312d0b1707cc6f2bef37f43f10ce2
toduchieu <toduchieu1245@gmail.com>
2022-10-30 15:24:57 +0700

    first commit

---
+++ Second-microservice/src/main/java/com/example/controller/SecondController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second")
+public class SecondController {
+	
+	@GetMapping("/message")
+    public String test() {
+        return "Hello this is my Second microservice";
+    }
+
+}
